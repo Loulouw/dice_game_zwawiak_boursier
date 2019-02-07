366825002e301cd69de891eb6baee51c39dea1be
Louis ZWAWIAK <louis.zwawiak@gmail.com>
2019-02-07 17:48:32 +0100

    interface jeu

--- dicegame/src/main/java/fr/miage/dicegame/App.java
+++ dicegame/src/main/java/fr/miage/dicegame/App.java
@@ -1,8 +1,45 @@
 package fr.miage.dicegame;
 
-public class App {
+import fr.miage.dicegame.ui.DeView;
+import fr.miage.dicegame.ui.View;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Tab;
+import javafx.scene.control.TabPane;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class App extends Application{
 	
 	public static void main(String[]args) {
-		
+		launch(args);
+	}
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		primaryStage.setTitle("Jeu de dé");
+		Pane root = new StackPane();
+
+		TabPane tabPane = new TabPane();
+
+		tabPane.getTabs().add(createTabPane("Jeu", new DeView()));
+
+		root.getChildren().add(tabPane);
+		primaryStage.setScene(new Scene(root, 500, 450));
+		primaryStage.show();
+
+	}
+
+	private static Tab createTabPane(String name, View view) {
+		final Tab tab = new Tab(name);
+		tab.selectedProperty().addListener((observable, oldValue, newValue) -> {
+			if (newValue) {
+				tab.setContent(view.getPane());
+			}
+		});
+		tab.setContent(view.getPane());
+		tab.setClosable(false);
+		return tab;
 	}
 }
--- dicegame/src/main/java/fr/miage/dicegame/ui/DeView.java
+++ dicegame/src/main/java/fr/miage/dicegame/ui/DeView.java
@@ -1,22 +1,58 @@
 package fr.miage.dicegame.ui;
 
-import java.awt.Label;
-import java.awt.Panel;
-import java.util.Observable;
-import java.util.Observer;
+import java.util.Optional;
 
-import fr.miage.dicegame.core.De;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Pane;
 
-public class DeView extends Panel implements Observer{
+public class DeView implements View {
 
-	private static final long serialVersionUID = 1L;
-	public Label l;
-	
-	public DeView(De de) {
-		this.add(l=new Label("De:" + de.getValeur()));
+	@Override
+	public Pane getPane() {
+		final GridPane gp = new GridPane();
+		gp.setAlignment(Pos.CENTER);
+		gp.setHgap(10);
+		gp.setVgap(10);
+		gp.setPadding(new Insets(25, 25, 25, 25));
+
+		final TextField nomJoueur = new TextField();
+
+		final Label lancerNumeroUn = new Label("Numéro du premier dé : X");
+		final Label lancerNumeroDeux = new Label("Numéro du second dé : X");
+
+		final Button jouer = new Button("Jouer");
+		jouer.setOnAction(value -> {
+			Alert a = new Alert(AlertType.CONFIRMATION);
+			a.setTitle("Confirmation");
+			a.setHeaderText("Voulez-vous lancer les dés ?");
+			Optional<ButtonType> result = a.showAndWait();
+			if (result.get() == ButtonType.OK) {
+				if (nomJoueur.getText().trim().isEmpty()) {
+					Alert a2 = new Alert(AlertType.WARNING);
+					a2.setTitle("Attention");
+					a2.setHeaderText("Le nom du joueur ne peut pas être vide");
+					a2.show();
+				} else {
+					// TODO: implementer jouer
+				}
+			}
+		});
+
+		gp.add(new Label("Nom du joueur "), 0, 0);
+		gp.add(nomJoueur, 1, 0);
+		gp.add(lancerNumeroUn, 0, 1);
+		gp.add(lancerNumeroDeux, 0, 2);
+		gp.add(jouer, 0, 3, 2, 1);
+
+		return gp;
 	}
-	
-	public void update(Observable o, Object arg) {
-		l.setText("De:"+((De)o).getValeur());
-	}
+
 }
